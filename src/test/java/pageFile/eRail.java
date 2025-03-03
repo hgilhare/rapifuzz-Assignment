eb46f9e36d6ab2ef42ef54e4f829f6dd44f34a0a
Himanshu Gilhare <himanshu.gilhare@tartanhq.com>
2025-03-03 13:23:51 +0530

    xpath fix

--- src/test/java/pageFile/eRail.java
+++ src/test/java/pageFile/eRail.java
@@ -29,41 +29,46 @@ import java.util.List;
 
 public class eRail {
     WebDriver driver;
-    By formLocator=By.cssSelector("input#txtStationFrom");
-    By fourthStationListLocator=By.xpath("(//div[@style='width:240px;float:left;overflow:hidden'])[4]");
-    By suggestedStaionlocator=By.xpath("(//div[@style='width:240px;float:left;overflow:hidden'])");
-    By dateLocator=By.xpath("(//input[@type='button'])[2]");
-    public void userOpenOnBrowser(String url){
-        driver= new ChromeDriver();
+    By formLocator = By.cssSelector("input#txtStationFrom");
+    By fourthStationListLocator = By.xpath("(//div[@style='width:240px;float:left;overflow:hidden'])[4]");
+    By suggestedStaionlocator = By.xpath("(//div[@style='width:240px;float:left;overflow:hidden'])");
+    By dateLocator = By.xpath("(//input[@type='button'])[2]");
+
+    public void userOpenOnBrowser(String url) {
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get(url);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
     }
-    public void userClickedOnFromField(){
 
+    public void userClickedOnFromField() {
         driver.findElement(formLocator).click();
     }
-    public void userClearDataFromField(){
+
+    public void userClearDataFromField() {
         driver.findElement(formLocator).clear();
     }
-    public void userEnterDataDelAndOpenedDropdown(){
+
+    public void userEnterDataDelAndOpenedDropdown() {
         driver.findElement(formLocator).sendKeys("DEL");
     }
-    public void userSelectFourthPositonAndPrintOnConsole(){
-       String fourthstation= driver.findElement(fourthStationListLocator).getText();
-        System.out.println(fourthstation);
+
+    public void userSelectFourthPositonAndPrintOnConsole() {
+        String fourthstation = driver.findElement(fourthStationListLocator).getText();
+        System.out.println("fourth station = " + fourthstation);
     }
-    public void userStoredAllSuggestedStationNameOnExecelSheetAndCompared()  {
-        List<WebElement> elements= driver.findElements(suggestedStaionlocator);
+
+    public void userStoredAllSuggestedStationNameOnExecelSheetAndCompared() {
+        List<WebElement> elements = driver.findElements(suggestedStaionlocator);
         Workbook workbook = new XSSFWorkbook();
-        Sheet sheet= workbook.createSheet("extracted data");
-        int rowNum=0;
-        for(WebElement element:elements){
-            Row row=sheet.createRow(rowNum++);
-            Cell cell=row.createCell(0);
+        Sheet sheet = workbook.createSheet("extracted data");
+        int rowNum = 0;
+        for (WebElement element : elements) {
+            Row row = sheet.createRow(rowNum++);
+            Cell cell = row.createCell(0);
             cell.setCellValue(element.getText());
         }
-        File f= new File("src/test/java/outputData/outputSheet.xlsx");
+        File f = new File("src/test/java/outputData/outputSheet.xlsx");
         f.delete();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         try (FileOutputStream fileOut = new FileOutputStream("src/test/java/outputData/outputSheet.xlsx")) {
@@ -73,29 +78,30 @@ public class eRail {
         }
 
         try {
-            FileInputStream extractedFile= new FileInputStream("src/test/java/outputData/outputSheet.xlsx");
-            FileInputStream defaultFile= new FileInputStream("src/test/java/defaultData/defaultStationName.xlsx");
-            Workbook w1= new XSSFWorkbook(extractedFile);
-            Workbook w2= new XSSFWorkbook(defaultFile);
-            Sheet s1=w1.getSheetAt(0);
-            Sheet s2=w2.getSheetAt(0);
+            FileInputStream extractedFile = new FileInputStream("src/test/java/outputData/outputSheet.xlsx");
+            FileInputStream defaultFile = new FileInputStream("src/test/java/defaultData/defaultStationName.xlsx");
+            Workbook w1 = new XSSFWorkbook(extractedFile);
+            Workbook w2 = new XSSFWorkbook(defaultFile);
+            Sheet s1 = w1.getSheetAt(0);
+            Sheet s2 = w2.getSheetAt(0);
 
-            if(s1.getLastRowNum()==s2.getLastRowNum()){
-                for(int i=0;i<rowNum;i++){
+            if (s1.getLastRowNum() == s2.getLastRowNum()) {
+                for (int i = 0; i < rowNum; i++) {
 
-                    Row r1= s1.getRow(i);
-                    Row r2= s2.getRow(i);
-                    Cell c1= r1.getCell(0);
-                    Cell c2= r2.getCell(0);
-                    if(c1.getStringCellValue().equals(c2.getStringCellValue())){
+                    Row r1 = s1.getRow(i);
+                    Row r2 = s2.getRow(i);
+                    Cell c1 = r1.getCell(0);
+                    Cell c2 = r2.getCell(0);
+                    if (c1.getStringCellValue().equals(c2.getStringCellValue())) {
                         Assert.assertTrue(true);
+                        System.out.println(c1.getStringCellValue() + " ======= " + c2.getStringCellValue());
                     } else {
-                        System.out.println("data not matched at index = "+i);
+                        System.out.println("data not matched at index = " + i);
                         Assert.assertTrue(true);
                     }
 
                 }
-            }else{
+            } else {
                 System.out.println("incorrect row count");
                 Assert.assertTrue(false);
             }
@@ -113,19 +119,24 @@ public class eRail {
 
 
     }
-    public void userSelectThirtyDayFromToday(){
+
+    public void userSelectThirtyDayFromToday() {
         driver.findElement(dateLocator).click();
-//
+
         Calendar calendar = Calendar.getInstance();
-        calendar.add(Calendar.DAY_OF_MONTH,30);
+        calendar.add(Calendar.DAY_OF_MONTH, 30);
         java.util.Date futureDate = calendar.getTime();
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
         String formattedDate = dateFormat.format(futureDate);
 
-        String d= formattedDate.substring(8,10);
 
-        String thirtyDayXpath="(//td[text()='"+d+"'])[1]";
-        By thirtydayLocator= By.xpath(thirtyDayXpath);
+        String d = formattedDate.substring(8, 10);
+        int date = Integer.parseInt(d);
+
+
+        String thirtyDayXpath = "(//td[text()='" + date + "'])[2]";
+        System.out.println(thirtyDayXpath);
+        By thirtydayLocator = By.xpath(thirtyDayXpath);
         driver.findElement(thirtydayLocator).click();
         try {
             Thread.sleep(5000);
